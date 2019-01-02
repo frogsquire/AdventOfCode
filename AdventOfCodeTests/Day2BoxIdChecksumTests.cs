968385cd7322e998ae83ac40227b3de664f9f6bc
Maxwell Zimon <max@zimon.co>
2019-01-02 15:02:27 -0600

    (day 2, part 2) add working solution and unit test

--- AdventOfCodeTests/Day2BoxIdChecksumTests.cs
+++ AdventOfCodeTests/Day2BoxIdChecksumTests.cs
@@ -18,6 +18,21 @@ namespace AdventOfCodeTests
             "ababab"
         };
 
+        private static readonly string[] TestSimilarIds = new[]
+        {
+            "abcde",
+            "fghij",
+            "klmno",
+            "pqrst",
+            "fguij",
+            "axcye",
+            "wvxyz"
+        };
+
+        private const string ExpectedSimilarChars = "fgij";
+        private const string ExpectedSimilarLine1 = "fghij";
+        private const string ExpectedSimilarLine2 = "fguij";
+
         // expected results
         private const int ExpectedTestChecksum = 12;
         private const int ExpectedTwoIdenticalCharacterLines = 4;
@@ -30,6 +45,15 @@ namespace AdventOfCodeTests
             Assert.AreEqual(ExpectedTestChecksum, result);
             Assert.AreEqual(ExpectedTwoIdenticalCharacterLines, twoIdenticalCharLines);
             Assert.AreEqual(ExpectedThreeIdenticalCharacterLines, threeIdenticalCharLines);
+        }
+
+        [Test]
+        public void GetCommonLettersBetweenIds_Returns_Correct_Letters_For_Test_Data()
+        {
+            var result = BoxIdChecksumLauncher.GetCommonLettersBetweenIds(TestSimilarIds, out string line1, out string line2);
+            Assert.AreEqual(ExpectedSimilarChars, result);
+            Assert.AreEqual(ExpectedSimilarLine1, line1);
+            Assert.AreEqual(ExpectedSimilarLine2, line2);
         }
     }
 }
--- Day2BoxIdChecksum/BoxIdChecksumLauncher.cs
+++ Day2BoxIdChecksum/BoxIdChecksumLauncher.cs
@@ -8,10 +8,13 @@ namespace AdventOfCode.Day2BoxIdChecksum
     {
         /*
          * Input: A set of strings, one per line, containing alphabet characters (non-alpha characters will be ignored). 
-         * Output: A 'checksum' which is calculated as follows:
+         * Output part 1: A 'checksum' which is calculated as follows:
          *  1. Let A be the number of strings where any letter appears exactly 3 times.
          *  2. Let B be the number of strings where any letter appears exactly 2 times.
          *  3. Return A * B.
+         * Output part 2: A string of characters common to two strings which represent sequential IDs. 
+         *  For example, if the input is ['abc','def','ajc','bab'] the output will be 'ac' because 'abc' and 'ajc' are identical save for one character.
+         *  (Only the first two such strings will be returned.)
          */
         public static void Main(string[] args)
         {
@@ -24,10 +27,17 @@ namespace AdventOfCode.Day2BoxIdChecksum
 
             Console.WriteLine("Processing " + path + " checksum...");
 
-            var result = GetChecksum(lines, out int twoIdenticalCharLines, out int threeIdenticalCharLines);
+            var checksumResult = GetChecksum(lines, out int twoIdenticalCharLines, out int threeIdenticalCharLines);
 
             Console.WriteLine(twoIdenticalCharLines + " strings with 2 of the same character; " + threeIdenticalCharLines + " strings with 3 of the same character.");
-            Console.WriteLine("Checksum: " + twoIdenticalCharLines * threeIdenticalCharLines);
+            Console.WriteLine("Checksum: " + checksumResult);
+
+            var commonLettersResult = GetCommonLettersBetweenIds(lines, out string line1, out string line2);
+
+            if (string.IsNullOrEmpty(commonLettersResult))
+                Console.WriteLine("No common IDs were found!");
+            else
+                Console.WriteLine("IDs " + line1 + " and " + line2 + " share characters " + commonLettersResult);
         }
 
         public static int GetChecksum(string[] lines, out int twoIdenticalCharLines, out int threeIdenticalCharLines)
@@ -36,14 +46,59 @@ namespace AdventOfCode.Day2BoxIdChecksum
             threeIdenticalCharLines = 0;
             foreach (var line in lines)
             {
-                // strip non-alpha characters and rebuild
-                // a regex might be simpler, but would be slower, and probably less readable, too
-                var alphaChars = Array.FindAll(line.ToCharArray(), c => char.IsLetter(c));
+                var alphaChars = GetAlphaChars(line);
                 var charCounts = alphaChars.GroupBy(c => c).Select(c => new { Char = c.Key, Count = c.Count() });
                 if (charCounts.Any(c => c.Count == 2)) twoIdenticalCharLines++;
                 if (charCounts.Any(c => c.Count == 3)) threeIdenticalCharLines++;
             }
             return twoIdenticalCharLines * threeIdenticalCharLines;
+        }
+
+        public static string GetCommonLettersBetweenIds(string[] lines, out string line1, out string line2)
+        {
+            // sort sequentially by characters and remove duplicates
+            var distinctLines = lines.Distinct();
+            var linesRemaining = distinctLines.ToList();
+            var length = distinctLines.ElementAt(0).Length; // assumption: all entries are same length
+
+            line1 = string.Empty; line2 = string.Empty;
+            foreach(var line in distinctLines)
+            {
+                linesRemaining.Remove(line);
+                // get candidate matches - strings that differ by exactly one character
+                var sharedChars = string.Empty;
+                var similarLine = linesRemaining.FirstOrDefault(l => 
+                {
+                    sharedChars = string.Empty;
+                    var lineChars = line.ToCharArray();
+                    var candidateChars = l.ToCharArray();
+                    var differenceSeen = false;
+                    for(var i = 0; i < length; i++)
+                    {
+                        if (lineChars[i] == candidateChars[i]) sharedChars += lineChars[i];
+                        else if (!differenceSeen) differenceSeen = true;
+                        else return false;
+                    }
+                    return true;
+                });
+
+                if (similarLine != null)
+                {
+                    line1 = line;
+                    line2 = similarLine;
+                    return sharedChars;
+                }
+            }
+
+            // none found
+            return string.Empty;            
+        }
+
+        // strip non-alpha characters and rebuild
+        // a regex might be simpler, but would be slower, and probably less readable, too
+        private static char[] GetAlphaChars(string str)
+        {
+            return Array.FindAll(str.ToCharArray(), c => char.IsLetter(c));
         }
     }
 }
